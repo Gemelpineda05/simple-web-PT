8d33b84723ede5ac3c9ad45dcf843c0086389bac
Gemelpineda05 <gemel.pineda@hcdc.edu.ph>
2024-12-20 16:00:16 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h4>Welcome to simple web site</h4>
+      <p>GEMEL PINEDA</p>
+    </div>
+  );
+}
+export default App;
+
+
+ 
